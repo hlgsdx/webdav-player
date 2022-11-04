dd967b63e7908c5e9e8b13e551fee04d88e078f7
SDX <20733681+hlgsdx@users.noreply.github.com>
2022-11-04 10:53:58 +0800

    feat: adjust window size
    also make it center on the screen

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -1,4 +1,5 @@
 import javafx.stage.Stage
+import javafx.stage.Screen
 import tornadofx.*
 import view.MainView
 import view.Styles
@@ -6,8 +7,11 @@ import view.Styles
 class Program : App(MainView::class, Styles::class) {
     override fun start(stage: Stage) {
         super.start(stage)
-        stage.width = 800.0
-        stage.height = 600.0
+        stage.width = 1000.0
+        stage.height = 640.0
+        val screenPrimary = Screen.getPrimary()
+        stage.x = screenPrimary.visualBounds.width - screenPrimary.visualBounds.width / 2 - stage.width / 2
+        stage.y = screenPrimary.visualBounds.height - screenPrimary.visualBounds.height / 2 - stage.height / 2
     }
 }
 
